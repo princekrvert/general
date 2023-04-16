3afedb84789892176d3c5346af87a440de3e45ad
PRINCE KUMAR <56459297+princekrvert@users.noreply.github.com>
2023-04-16 09:34:14 +0000

    sendinblue api added
    send in blue api added to the general

---
+++ sendinblue/sendinblue.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+import time
+import sib_api_v3_sdk
+from sib_api_v3_sdk.rest import ApiException
+from pprint import pprint
+
+configuration = sib_api_v3_sdk.Configuration()
+configuration.api_key['api-key'] = 'your_api_here'
+
+api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
+subject = "My Subject"
+html_content = "<html><body><h1>kaiso ho  </h1></body></html>"
+sender = {"name":"Prince","email":"Example.com"}
+to = [{"email":"example.com","name":"Jane Doe"}]
+cc = [{"email":"example2@example2.com","name":"Janice Doe"}]
+bcc = [{"name":"John Doe","email":"example@example.com"}]
+reply_to = {"email":"replyto@domain.com","name":"John Doe"}
+headers = {"Some-Custom-Name":"unique-id-1234"}
+params = {"parameter":"My param value","subject":"HI there"}
+send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(to=to, bcc=bcc, cc=cc, reply_to=reply_to, headers=headers, html_content=html_content, sender=sender, subject=subject)
+
+try:
+    api_response = api_instance.send_transac_email(send_smtp_email)
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling SMTPApi->send_transac_email: %s\n" % e)
